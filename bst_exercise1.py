8c545cbbfa7f35689388272950636a84ea48c45b
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-14 10:35:19 +0800

    BST node class implemented.

---
+++ bst_exercise1.py
@@ -0,0 +1,6 @@
+# Starting from creating a class for binary tree properties
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
