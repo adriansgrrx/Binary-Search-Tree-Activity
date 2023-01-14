16f98f16d1394108e99a0c7e504cc055161f82cd
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-14 11:24:56 +0800

    Build_tree metho implemented. Output tested for in order traversal.

--- bst_exercise1.py
+++ bst_exercise1.py
@@ -35,7 +35,7 @@ class BinarySearchTreeNode:
                 return self.right.search(val)
             else:
                 return False
-
+    
     def in_order_traversal(self):
             elements = []
             if self.left:
@@ -47,3 +47,21 @@ class BinarySearchTreeNode:
                 elements += self.right.in_order_traversal()
 
             return elements
+
+def build_tree(elements):
+    print("Building tree with these elements:",elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
+    country_tree = build_tree(countries)
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+
+    print("UK is in the list? ", country_tree.search("UK"))
+    print("Sweden is in the list? ", country_tree.search("Sweden"))
+    print("In order traversal gives this sorted list:",numbers_tree.in_order_traversal())
