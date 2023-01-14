c9b8ab49136b09f42d421371d742a403f6bd256a
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-14 10:47:51 +0800

    add_child method implemented. Added some take aways.

--- bst_exercise1.py
+++ bst_exercise1.py
@@ -4,3 +4,18 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+    
+    def add_child(self, data):
+        if data == self.data: # this condition will prevent having the same child/node
+            return
+
+        if data < self.data: # this condition will put the child to the left
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else: # this condition will put the child to the right
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
